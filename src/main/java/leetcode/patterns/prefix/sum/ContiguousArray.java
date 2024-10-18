cf631840ca7e7f1f05547d7e32dd276162ea04de
Sonali Goel <112942761+goelsonali@users.noreply.github.com>
2024-10-18 20:48:57 +0100

    leetcode prefix sum

---
+++ src/main/java/leetcode/patterns/prefix/sum/ContiguousArray.java
@@ -0,0 +1,18 @@
+package leetcode.patterns.prefix.sum;
+
+//525. Contiguous Array
+// Given a binary array nums, return the maximum length of a contiguous subarray with an equal number of 0 and 1.
+//Input: nums = [0,1]
+//Output: 2
+//Explanation: [0, 1] is the longest contiguous subarray with an equal number of 0 and 1.
+//Input: nums = [0,1,0,0,1]
+// Output: 2
+// Explanation: [0, 1] (or [1, 0]) is a longest contiguous subarray with equal number of 0 and 1.
+
+
+public class ContiguousArray {
+
+    public int findMaxLength(int[] nums) {
+        return 0;
+    }
+}
