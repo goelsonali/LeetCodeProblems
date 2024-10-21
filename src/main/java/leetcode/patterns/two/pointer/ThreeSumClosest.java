8d4a8a4ef30f94defc5f39cdc90dcbe691e7a1da
Sonali Goel <112942761+goelsonali@users.noreply.github.com>
2024-10-21 22:12:28 +0100

    leetcode 2 pointer problem

---
+++ src/main/java/leetcode/patterns/two/pointer/ThreeSumClosest.java
@@ -0,0 +1,20 @@
+package leetcode.patterns.two.pointer;
+
+//16. 3Sum Closest
+//Given an integer array nums of length n and an integer target, find three integers in nums such that the sum is closest to target.
+//
+//Return the sum of the three integers.
+//
+//You may assume that each input would have exactly one solution.
+//Input: nums = [-1,2,1,-4], target = 1
+//Output: 2
+//Explanation: The sum that is closest to the target is 2. (-1 + 2 + 1 = 2).
+//Input: nums = [0,0,0], target = 1
+//Output: 0
+//Explanation: The sum that is closest to the target is 0. (0 + 0 + 0 = 0).
+public class ThreeSumClosest {
+
+    public static int threeSumClosest(int[] nums, int target) {
+        return 0;
+    }
+}
