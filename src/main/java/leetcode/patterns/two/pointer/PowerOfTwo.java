dbd6728f3d6962884f06287d235b4d36603ae2d9
Sonali Goel <112942761+goelsonali@users.noreply.github.com>
2024-10-24 21:24:31 +0100

    leetcode 2 pointer power of two

---
+++ src/main/java/leetcode/patterns/two/pointer/PowerOfTwo.java
@@ -0,0 +1,23 @@
+package leetcode.patterns.two.pointer;
+//231. Power of Two
+
+//Given an integer n, return true if it is a power of two. Otherwise, return false.
+//
+//An integer n is a power of two, if there exists an integer x such that n == 2(power of x).
+
+//Input: n = 1
+//Output: true
+//Explanation: 2(power of 0) = 1
+
+//Input: n = 16
+//Output: true
+//Explanation: 2(power of 4) = 16
+
+//Input: n = 3
+//Output: false
+public class PowerOfTwo {
+
+    public static boolean isPowerOfTwo(int n) {
+        return false;
+    }
+}
