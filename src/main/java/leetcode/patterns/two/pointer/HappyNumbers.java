a1e6c514afe9440397cd767a4f4a706cbc86b47e
Sonali Goel <112942761+goelsonali@users.noreply.github.com>
2024-10-28 10:49:20 +0000

    leetcode 2 pointer happy number

--- src/main/java/leetcode/patterns/two/pointer/HappyNumbers.java
+++ src/main/java/leetcode/patterns/two/pointer/HappyNumbers.java
@@ -10,16 +10,38 @@ package leetcode.patterns.two.pointer;
 //Input: n = 19
 //Output: true
 //Explanation:
-//12 + 92 = 82
-//82 + 22 = 68
-//62 + 82 = 100
-//12 + 02 + 02 = 1
+//1pow(2)+ 9pow(2) = 82
+//8pow(2) + 2pow(2) = 68
+//6pow(2) + 8pow(2) = 100
+//1pow(2) + 0pow(2) + 0pow(2)= 1
+
+import java.util.HashSet;
+import java.util.Set;
 
 //Input: n = 2
 //Output: false
 public class HappyNumbers {
 
     public static boolean isHappy(int n) {
-        return true;
+        Set<Integer> seen = new HashSet<>();
+        while (n != 1 && !seen.contains(n)) {
+            seen.add(n);
+            n = getNext(n);
+        }
+        return n == 1;
+    }
+
+    private static int getNext(int n) {
+        int totalSum = 0;
+        while (n > 0) {
+            int digit = n % 10;
+            totalSum += digit * digit;
+            n /= 10;
+        }
+        return totalSum;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isHappy(19));
     }
 }
